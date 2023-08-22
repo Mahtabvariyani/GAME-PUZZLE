ee89998639294ecadffbc48522eddf7c04fc0602
Mah <mahtabvariyani@gmail.com>
2023-08-22 19:57:26 +0200

    Add Final Project

---
+++ README.md
@@ -0,0 +1,18 @@
+Picture Puzzle Game
+This is a simple puzzle game that lets you solve a jigsaw puzzle with Avengers-themed images. The game is built using HTML, CSS, and JavaScript.
+
+Game Board: The game board consists of a grid of empty tiles and puzzle pieces.
+
+Drag and Drop: Click and drag a puzzle piece from the gallery on the right-hand side and drop it onto an empty tile on the game board to swap positions.
+
+Objective: Rearrange the puzzle pieces to form the complete Avengers-themed image.
+
+Turns: The number of turns you take to solve the puzzle is displayed at the top. You have a maximum of 30 turns to solve the puzzle.
+
+Background Music: You can play and pause the background music using the play button at the top right corner of the page.
+
+How to Contribute
+This is a simple project meant for Practice JavaScript and CSS.
+
+Credits
+This puzzle game is developed using HTML, CSS, and JavaScript.
---
+++ puzzle.js
@@ -0,0 +1,207 @@
+let audio = document.getElementById("audio");
+let playbtn = document.getElementById("playBtn");
+let count = 0;
+
+function play() {
+  if (count == 0) {
+    count = 1;
+    audio.play();
+  } else {
+    count = 0;
+    audio.pause();
+  }
+}
+
+const colors = ["#FBDF2C", "#FB2C74", "#2CD5FB", "#FFD92E", "#F95AEF"];
+
+const numBalls = 30;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12,
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` },
+    ],
+    {
+      duration: (Math.random() + 1) * 2000,
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out",
+    }
+  );
+});
+
+/* function Notice(){
+    alert("Game Rules"+
+    ":You have Maximum Of 30 Moves to Fix the PuZzle!!!"
+    
+    )
+};
+Notice()  
+   */
+
+var rows = 5;
+var columns = 5;
+
+var currTile;
+var otherTile;
+
+var turns = 0;
+
+window.onload = function () {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      //<img>
+      let tile = document.createElement("img");
+
+      if ((tile.src = "./IconBackground/blank2.jpg")) {
+        tile.style.opacity = "0.5";
+      }
+
+      tile.addEventListener("dragstart", dragStart);
+      tile.addEventListener("dragover", dragOver);
+      tile.addEventListener("dragenter", dragEnter);
+      tile.addEventListener("dragleave", dragLeave);
+      tile.addEventListener("drop", dragDrop);
+      tile.addEventListener("dragend", dragEnd);
+
+      document.getElementById("board").append(tile);
+    }
+  }
+
+  //pieces
+  let pieces = [];
+  for (let i = 1; i <= rows * columns; i++) {
+    pieces.push(i.toString());
+  }
+  pieces.reverse();
+  for (let i = 0; i < pieces.length; i++) {
+    let j = Math.floor(Math.random() * pieces.length);
+
+    let tmp = pieces[i];
+    pieces[i] = pieces[j];
+    pieces[j] = tmp;
+  }
+
+  for (let i = 0; i < pieces.length; i++) {
+    let tile = document.createElement("img");
+    tile.src = "./images/" + pieces[i] + ".jpg";
+
+    tile.addEventListener("dragstart", dragStart);
+    tile.addEventListener("dragover", dragOver);
+    tile.addEventListener("dragenter", dragEnter);
+    tile.addEventListener("dragleave", dragLeave);
+    tile.addEventListener("drop", dragDrop);
+    tile.addEventListener("dragend", dragEnd);
+
+    document.getElementById("gall").append(tile);
+  }
+};
+
+function dragStart() {
+  currTile = this;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+}
+
+function dragLeave() {}
+
+function dragDrop() {
+  otherTile = this;
+}
+let turnbtn = document.getElementById("turns");
+function MaxTurn() {
+  if (turns >= 30) {
+    turnbtn.style.backgroundColor = "#c765a2";
+    CustomAlert(".You made more than 30 moves.", "Game Over!");
+  }
+}
+
+function dragEnd() {
+  if (currTile.src.includes("blank")) {
+    return;
+  }
+
+  let currImg = currTile.src;
+  let otherImg = otherTile.src;
+
+  currTile.src = otherImg;
+  otherTile.src = currImg;
+
+  otherTile.style.opacity = "1";
+
+  turns += 1;
+  document.getElementById("turns").innerText = turns;
+  MaxTurn();
+}
+
+//____________________
+function CustomAlert(message, title,) {
+  console.log("triggered");
+  this.alert = function (message, title) {
+    document.body.innerHTML =
+      document.body.innerHTML +
+      '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
+
+    let dialogoverlay = document.getElementById("dialogoverlay");
+    let dialogbox = document.getElementById("dialogbox");
+
+    let winH = window.innerHeight;
+    dialogoverlay.style.height = winH + "px";
+
+    dialogbox.style.top = "100px";
+
+    dialogoverlay.style.display = "block";
+    dialogbox.style.display = "block";
+
+    document.getElementById("dialogboxhead").style.display = "block";
+
+    if (typeof title === "undefined") {
+      document.getElementById("dialogboxhead").style.display = "none";
+    } else {
+      document.getElementById("dialogboxhead").innerHTML =
+        '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' + title;
+    }
+    document.getElementById("dialogboxbody").innerHTML = message;
+    document.getElementById("dialogboxfoot").innerHTML =
+      '<button class="pure-material-button-contained active" onclick="refreshPage()">Refresh</button>';
+  };
+
+  this.ok = function () {
+    document.getElementById("dialogbox").style.display = "none";
+    document.getElementById("dialogoverlay").style.display = "none";
+  };
+  this.alert(message, title);
+}
+
+function refreshPage() {
+  window.location.reload();
+}
